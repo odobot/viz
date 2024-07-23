e844ca64b508dadddcc6c4cb2756456957072f9e
odobot <ndibapeter4@gmail.com>
2024-07-23 21:19:11 +0300

    Weuh, tayaaaad!

--- mpu_viz/launch/imu_demo.launch.py
+++ mpu_viz/launch/imu_demo.launch.py
@@ -1,80 +1,64 @@
+#!/usr/bin/env python3
+
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+import launch_ros.actions
 
 def generate_launch_description():
-    # Declare launch arguments
-    model_arg = DeclareLaunchArgument(
-        name='model',
-        default_value=os.path.join(get_package_share_directory('mpu_viz'), 'urdf', 'plane.urdf'),
-        description='Full path to the URDF file to be loaded'
-    )
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    turtlebot3_cartographer_prefix = get_package_share_directory('mpu_viz')
+    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
+                                                  turtlebot3_cartographer_prefix, 'config'))
+    configuration_basename = LaunchConfiguration('configuration_basename',
+                                                 default='turtlebot3_lds_2d.lua')
 
-    gui_arg = DeclareLaunchArgument(
-        name='gui',
-        default_value='true',
-        description='Flag to enable GUI'
-    )
+    resolution = LaunchConfiguration('resolution', default='0.05')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
 
-    robot_description_param = DeclareLaunchArgument(
-        name='robot_description',
-        default_value=os.path.join(get_package_share_directory('mpu_viz'), 'urdf', 'plane.urdf'),
-        description='Full path to the URDF file to be loaded'
-    )
+    rviz_config_dir = os.path.join(get_package_share_directory('mpu_viz'),
+                                   'rviz', 'tb3_cartographer.rviz')
+    
+    urdf_file = os.path.join(
+        get_package_share_directory('mpu_viz'), 'urdf', 'plane.urdf')
 
-    # Define nodes
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': LaunchConfiguration('robot_description')}]
-    )
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'urdf_file',
+            default_value=os.path.join(get_package_share_directory('mpu_viz'), 'urdf', 'plane.urdf'),
+            description='Full path to the URDF file to be loaded'
+        ),
+        
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('urdf_file')])}],
+        ),
 
-    imu_node = Node(
-        package='mpu_viz',
-        executable='imu_node.py',
-        name='imu_node',
-        output='screen'
-    )
+        Node(
+            package='mpu_viz', 
+            executable='imu_node', 
+            output='screen'),
+            
+        Node(
+            package='mpu_viz', 
+            executable='tf_broadcaster_imu', 
+            output='screen')
 
-    imu_filter_node = Node(
-        package='imu_complementary_filter',
-        executable='complementary_filter_node',
-        name='imu_filter_node_for_orientation',
-        output='screen'
-    )
+        
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     name='rviz2',
+        #     # arguments=['-d', rviz_config_dir],
+        #     parameters=[{'use_sim_time': use_sim_time}],
+        #     output='screen'),
+    ])
 
-    tf_broadcaster_node = Node(
-        package='mpu_viz',
-        executable='tf_broadcaster_imu.py',
-        name='rpy_tf',
-        output='screen'
-    )
-
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        arguments=['-d', LaunchConfiguration('rviz_config')],
-        output='screen'
-    )
-
-    # Create the launch description
-    ld = LaunchDescription()
-
-    # Add all actions to the launch description
-    ld.add_action(model_arg)
-    ld.add_action(gui_arg)
-    ld.add_action(robot_description_param)
-    ld.add_action(robot_state_publisher_node)
-    ld.add_action(imu_node)
-    ld.add_action(imu_filter_node)
-    ld.add_action(tf_broadcaster_node)
-    # Uncomment the following line if RViz configuration is ready
-    # ld.add_action(rviz_node)
-
-    return ld
